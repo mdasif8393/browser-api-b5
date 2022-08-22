05c101638e5c21c8e5373bc579655f0b5059358b
mdasif8393 <md.asif.8393@gmail.com>
2022-08-22 16:46:21 +0600

    add local storage

---
+++ alert.js
@@ -0,0 +1,13 @@
+const showAlert = () =>{
+    alert("Maa is coming")
+}
+
+const showConfirm = () =>{
+    const response = confirm("Are you sure"); 
+    console.log(response);
+}
+
+const askName = () =>{
+    const name = prompt("What is your Name?");
+    console.log(name);
+}
